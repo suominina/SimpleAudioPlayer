4e22558d163deac11c47337004a0030209a94fb3
suominina <y.est.kren.f@gmail.com>
2024-12-01 13:00:52 +0900

    some changes

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@
 #include <SDL.h>
 #include <SDL_mixer.h>
 
-void scc(int code)
+void check_code(int code)
 {
     if (code < 0) {
         fprintf(stderr, "SDL_ERROR: %s\n", SDL_GetError());
@@ -15,7 +15,7 @@ void scc(int code)
     }
 }
 
-void *scp(void *ptr)
+void *check_ptr(void *ptr)
 {
     if (ptr == NULL) {
         fprintf(stderr, "SDL_ERROR: %s\n", SDL_GetError());
@@ -25,37 +25,32 @@ void *scp(void *ptr)
     return ptr;
 }
 
-void play_music(char *path)
+/* Play dropped music, return the music title. */
+char *play_music(char *dropped_file, Mix_Music *music)
 {
-//     char *dropped_file;
-//     Mix_Music *music;
-//     int frequency = 48000; /* Reasonable default according to SDL_mixer.h */
-//     Uint16 format = AUDIO_S16SYS;
-//     int channels = 2; /* 1 for mono, 2 for stereo */
-//     int chunksize = 2048; /* Reasonable default according to SDL_mixer.h */
-// 
-//     scc(Mix_OpenAudio(frequency, format, channels, chunksize));
-//     music = scp(Mix_LoadMUS(dropped_file));
-//     scc(Mix_PlayMusic(music, 0));
-// 
-//     if (dropped_file) { SDL_free(dropped_file); }
-//     if (music) { Mix_FreeMusic(music); }
+     Uint16 format = AUDIO_S16SYS;
+     int frequency = 48000; /* Reasonable default according to SDL_mixer.h */
+     int channels = 2; /* 1 for mono, 2 for stereo */
+     int chunksize = 2048; /* Reasonable default according to SDL_mixer.h */
+ 
+     check_code(Mix_OpenAudio(frequency, format, channels, chunksize));
+     music = check_ptr(Mix_LoadMUS(dropped_file));
+     check_code(Mix_PlayMusic(music, 0));
+
+     return Mix_GetMusicTitle(music);
+
 }
 
 int main(int argc, char **argv)
 {
     char *dropped_file;
     Mix_Music *music;
-    int frequency = 48000; /* Reasonable default according to SDL_mixer.h */
-    Uint16 format = AUDIO_S16SYS;
-    int channels = 2; /* 1 for mono, 2 for stereo */
-    int chunksize = 2048; /* Reasonable default according to SDL_mixer.h */
 
-    scc(SDL_Init(SDL_INIT_VIDEO));
-    scc(Mix_Init(MIX_INIT_MP3 | MIX_INIT_FLAC | MIX_INIT_WAVPACK));
+    check_code(SDL_Init(SDL_INIT_EVERYTHING));
+    check_code(Mix_Init(MIX_INIT_MP3 | MIX_INIT_FLAC | MIX_INIT_WAVPACK));
 
     SDL_Window *window = 
-        scp(SDL_CreateWindow("Music Player", 
+        check_ptr(SDL_CreateWindow("Music Player", 
                          SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                          800, 450, SDL_WINDOW_RESIZABLE));
 
@@ -73,17 +68,13 @@ int main(int argc, char **argv)
             case SDL_DROPFILE:
                 dropped_file = event.drop.file;
                 if (dropped_file) {
-                    scc(Mix_OpenAudio(frequency, format, channels, chunksize));
-                    music = scp(Mix_LoadMUS(dropped_file));
-                    scc(Mix_PlayMusic(music, 0));
-
-                    const char *now_playing = Mix_GetMusicTitle(music);
+                    char *now_playing = play_music(dropped_file, music);
                     printf("Now playing... %s\n", now_playing);
                     fflush(stdout);
                 } else {
                     fprintf(stderr, "Failed to play music.\n");
                     break;
-                };
+                }
                 break;
             }
         }
@@ -91,7 +82,6 @@ int main(int argc, char **argv)
     SDL_Delay(0);
 
     if (dropped_file) { SDL_free(dropped_file); }
-    if (music) { Mix_FreeMusic(music); }
     SDL_DestroyWindow(window);
     Mix_Quit();
     SDL_Quit();
